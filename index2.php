3046006b8d8d4dd837778807dfa07ffcd61d4239
Chicoiins <franciscolorenssutedossantos@gmail.com>
2024-10-17 10:43:19 -0300

    Renomeando os php

--- index2.php
+++ index2.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício 1 - 1</title>
+    <title>Exercício 1 - 2</title>
 </head>
 <body>
     <?php
-    echo '<img src="https://picsum.photos/500/600" alt="imagen de exemplo"></br>';
-    print "<h1>Olá Mundo!<h1> ";
+    print '<h1>URLs Absolutas</h1>'
+
+    print '<h1>URLs relativas</h1>'
     ?>
 </body>
 </html>
