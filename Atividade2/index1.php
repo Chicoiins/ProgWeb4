a20ffaac5cdb53c9398fca3c1dd17ccac32075e3
Chicoiins <franciscolorenssutedossantos@gmail.com>
2024-10-24 10:59:35 -0300

    correçaõ do codigo php

--- Atividade2/index1.php
+++ Atividade2/index1.php
@@ -9,22 +9,22 @@
     <?php
     $datetime = new DateTime("now", new DateTimeZone("America/Sao_Paulo"));
     $hora = $datetime->format('H');
-    if (H >= 0 && H <= 12)
+    if ($hora >= 0 and $hora <= 12)
     {
         print "<h1>Bom dia!</h1>";
-        echo '<img src="https://media-cdn.tripadvisor.com/media/photo-s/10/b1/37/87/o-lindo-nascer-do-sol.jpg" alt="imagem de exemplo">';
+        echo '<img src="img/bomdia.jpg" alt="imagem de exemplo">';
     }
         
-    elseif (H > 12 && H < 18)
+    elseif ($hora > 12 && $hora < 18)
     {
         print "<h1>Boa tarde!</h1>";
-        echo '<img src="https://images.pexels.com/photos/66997/pexels-photo-66997.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="imagem de exemplo">';
+        echo '<img src="img/boa-tarde.jpg" alt="imagem de exemplo">';
     }
         
-    elseif (H >= 18 && H < 24)
+    elseif ($hora >= 18 && $hora < 24)
     {
         print "<h1>Boa noite!</h1>";
-        echo '<img src="https://marcellocavalcanti.com.br/wp-content/uploads/2018/07/mc-504.jpg" alt="imagem de exemplo">';
+        echo '<img src="img/boa-noite.jpg" alt="imagem de exemplo">';
     }
     ?>
 
